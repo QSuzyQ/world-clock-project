397bdd40f2f769cd4781da3a0b88706a5ba3eb2d
QSuzyQ <92366704+QSuzyQ@users.noreply.github.com>
2022-10-30 22:23:50 +0100

    Added function to update time on every second

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
-//Istanbul
-let istanbulElement = document.querySelector("#istanbul");
-let istanbulDateElement = istanbulElement.querySelector(".date");
-let istanbulTimeElement = istanbulElement.querySelector(".time");
-let istanbulTime = moment().tz("Asia/Istanbul");
-istanbulDateElement.innerHTML = istanbulTime.format("MMMM Do, YYYY");
-istanbulTimeElement.innerHTML = istanbulTime.format(
-  "h:mm:ss [<span>]A[</span>]"
-);
+function updateTime() {
+  //Istanbul
+  let istanbulElement = document.querySelector("#istanbul");
+  let istanbulDateElement = istanbulElement.querySelector(".date");
+  let istanbulTimeElement = istanbulElement.querySelector(".time");
+  let istanbulTime = moment().tz("Asia/Istanbul");
+  istanbulDateElement.innerHTML = istanbulTime.format("MMMM Do, YYYY");
+  istanbulTimeElement.innerHTML = istanbulTime.format(
+    "h:mm:ss [<span>]A[</span>]"
+  );
+
+  //Vienna
+  let viennaElement = document.querySelector("#vienna");
+  let viennaDateElement = viennaElement.querySelector(".date");
+  let viennaTimeElement = viennaElement.querySelector(".time");
+  let viennaTime = moment().tz("Europe/Vienna");
+  viennaDateElement.innerHTML = viennaTime.format("MMMM Do, YYYY");
+  viennaTimeElement.innerHTML = viennaTime.format("h:mm:ss [<span>]A[</span>]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
